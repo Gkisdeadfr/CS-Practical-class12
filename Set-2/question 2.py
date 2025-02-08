06ace8dce68ddb6b1095efc7d38f3d22d81f5ea6
yjisdeadfr <yashasvijain2509@gmail.com>
2025-02-08 22:02:56 +0530

    Create question 2.py

---
+++ Set-2/question 2.py
@@ -0,0 +1,6 @@
+"""
+To display all records from FACULTY table whose Hire date is more than 05-oct-2001.
+To display F_ID, Fname, Cname of those faculties who charged more than 15000 as fees.
+Display count of all records from COURSES table grouped by F_ID.
+Display all records FACULTY table order by First Name of the faculty in descending order.
+"""
